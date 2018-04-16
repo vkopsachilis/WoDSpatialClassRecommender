25e342ae08b5335fa1bde5b681d6601a40f3d733
UoA-CartoGILab <31096410+vkopsachilis@users.noreply.github.com>
2018-04-16 20:00:59 +0300

    Create IdentifySpatialDatasets.java

---
+++ IdentifySpatialDatasets.java
@@ -0,0 +1,7 @@
+	public static String askforWGS84			="ASK {?s <http://www.w3.org/2003/01/geo/wgs84_pos#lat> ?o}";
+	public static String askforNeoGeo			="ASK {?s <http://geovocab.org/geometry#geometry> ?o}";
+	public static String askforGeoSparqlWKT		="ASK {?s <http://www.opengis.net/ont/geosparql#asWKT> ?o}";
+	public static String askforGeoSparqlGML		="ASK {?s <http://www.opengis.net/ont/geosparql#asGML> ?o}";
+	public static String askforGeoRSS			="ASK {?s <http://www.georss.org/georss/point> ?o}";
+	public static String askforOS				="ASK {?s <http://data.ordnancesurvey.co.uk/ontology/geometry/asGML> ?o}";
+	public static String askforWGS84geometry	="ASK {?s <http://www.w3.org/2003/01/geo/wgs84_pos#geometry> ?o}";
